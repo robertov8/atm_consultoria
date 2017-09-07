77eac8a9db071fc663da7a6700b31cd1e55e0fe7
Roberto Ribeiro <robertov82008@gmail.com>
2017-09-07 16:04:52 -0300

    Section 10, class 128

--- src/components/CenaClientes.js
+++ src/components/CenaClientes.js
@@ -24,16 +24,41 @@ export default class CenaClientes extends Component {
 
                 <BarraNavegacao />
 
-                <Image source={detalhesClientes} />
-                <Text>Nossos Clientes</Text>
+                <View style={styles.cabecalho}>
+                    <Image source={detalhesClientes} />
+                    <Text style={styles.txtTitulo}>Nossos Clientes</Text>
+                </View>
 
-                <Image source={cliente1} />
-                <Text>Lorem ipsum dolorem</Text>
-
-
-                <Image source={cliente2} />
-                <Text>Lorem ipsum dolorem</Text>
+                <View style={styles.detalhesClientes}>
+                    <Image source={cliente1} />
+                    <Text style={styles.txtdetalhesClientes}>Lorem ipsum dolorem</Text>
+                </View>
+                <View style={styles.detalhesClientes}>
+                    <Image source={cliente2} />
+                    <Text style={styles.txtdetalhesClientes}>Lorem ipsum dolorem</Text>
+                </View>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    cabecalho: {
+        marginTop: 20,
+        flexDirection: 'row'
+    },
+    txtTitulo: {
+        fontSize: 30,
+        color: '#B9C941',
+        marginLeft: 10,
+        marginTop: 25
+    },
+    detalhesClientes: {
+        padding: 20,
+        marginTop: 10
+    },
+    txtdetalhesClientes: {
+        fontSize: 18,
+        marginLeft: 20
+    }
+});
