52b85af1f79d5490f5c8bd54ccb6efde87f33a86
Roberto Ribeiro <robertov82008@gmail.com>
2017-09-07 17:33:14 -0300

    Section 10, class 135

---
+++ src/components/CenaContatos.js
@@ -0,0 +1,51 @@
+// @flow
+import React, { Component } from 'react';
+import { View, Image, Text } from 'react-native';
+
+import BarraNavegacao from './BarraNavegacao';
+
+const detalheContatos = require('../img/detalhe_servico.png');
+
+export default class CenaContatos extends Component {
+    render() {
+        return (
+            <StatusBar
+                backgroundColor='#CCC'
+            />
+
+            <BarraNavegacao voltar navigator={this.props.navigator} />
+
+            <View style={styles.cabecalho}>
+                <Image source={detalheContatos} />
+                <Text style={style.txtTitulo}>Contatos</Text>
+            </View>
+
+            <View style={styles.detalhesContatos}>
+                <Text style={styles.txtContatos}>TEL: (11) 1234-1234</Text>
+                <Text style={styles.txtContatos}>CEL: (11) 91234-1234</Text>
+                <Text style={styles.txtContatos}>EMAIL: contato@atmconsultoria.com</Text>
+            </View>
+        );
+    }
+}
+
+
+const styles = StyleSheet.create({
+    cabecalho: {
+        flexDirection: 'row',
+        marginTop: 20
+    },
+    txtTitulo: {
+        fontSize: 30,
+        color: '#61BD8C',
+        marginLeft: 10,
+        marginTop: 25
+    },
+    detalheContatos: {
+        marginTop: 20,
+        padding: 20
+    },
+    txtContatos: {
+        fontSize: 18
+    }
+});
